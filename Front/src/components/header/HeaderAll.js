fcf0a1afe072593e96388d9387b396ce1f19810b
Shiffff <alexis.privat@live.fr>
2022-10-03 21:03:03 +0200

    isadmin

--- Front/src/components/header/HeaderAll.js
+++ Front/src/components/header/HeaderAll.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { accountService } from "../../services/account_service";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 
 const HeaderAll = () => {
+  const [IsAdmin, setIsAdmin] = useState(false);
+  const userData = useSelector((state) => state.user.user);
   let navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAdmin = () => {
+      if (userData.isAdmin === true) {
+        setIsAdmin(true);
+      }
+    };
+    checkAdmin();
+  }, [userData]);
 
   const logout = () => {
     accountService.logout();
@@ -15,6 +28,10 @@ const HeaderAll = () => {
       <nav>
         <ul>
           <li>LOGO</li>
+          <li>
+            <NavLink to="/">Accueil</NavLink>
+          </li>
+          {IsAdmin && <NavLink to="/admin/dashboard">Admin_page</NavLink>}
           <li>
             <button onClick={logout}>Logout</button>
           </li>
